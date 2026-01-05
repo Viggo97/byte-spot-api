b738aaa553608c25c80ecc7bd08181570e839469
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-01-05 19:06:07 +0100

    restore config

--- api/src/db/postgresql.js
+++ api/src/db/postgresql.js
@@ -1,18 +1,12 @@
 import { Pool } from 'pg';
 
-const config = process.env.DB_CONNECTION_STRING
-    ? {
-        connectionString: process.env.DB_CONNECTION_STRING
-    }
-    : {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-    }
-
-const pool = new Pool(config);
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+});
 
 export const query = (text, params) => {
     return pool.query(text, params)
