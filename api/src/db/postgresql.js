b03e94841fb00f0988a4d70b149c698c43c2d0fd
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-01-05 18:22:41 +0100

    add connection string

--- api/src/db/postgresql.js
+++ api/src/db/postgresql.js
@@ -1,12 +1,18 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-});
+const config = process.env.DB_CONNECTION_STRING
+    ? {
+        connectionString: process.env.DB_CONNECTION_STRING
+    }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+    }
+
+const pool = new Pool(config);
 
 export const query = (text, params) => {
     return pool.query(text, params)
